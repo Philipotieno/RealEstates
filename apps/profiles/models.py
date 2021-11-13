200892c418dd524a22aab86b7bc90e770d405dac
philipotieno <iamphilipotieno@gmail.com>
2021-11-13 15:33:45 +0300

    feat: add profile models

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from apps.common.models import TimeStampedUUID
+
+User = get_user_model()
+
+
+class Gender(models.TextChoices):
+    Male = "Male", _("Male")
+    Female = "Female", _("Female")
+    Other = "Other", _("Other")
+
+
+class Profile(TimeStampedUUID):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"), max_length=30, default="+254703473377"
+    )
+    about_me = models.TextField(
+        verbose_name=_("About Me"), default="say something about yourself"
+    )
+    license = models.CharField(
+        verbose_name=_("Real Estate License"), max_length=20, blank=True, null=True
+    )
+    profile_photo = models.ImageField(
+        verbose_name=_("Profile Photo"), default="/profile_default.png"
+    )
+    gender = models.CharField(
+        verbose_name="Gender",
+        choices=Gender.choices,
+        default=Gender.Other,
+        max_length=20,
+    )
+    country = CountryField(
+        verbose_name=_("Country"), default="KE", max_length=20, blank=False, null=False
+    )
+
+    city = models.CharField(
+        verbose_name=_("City"),
+        default="Nairobi",
+        max_length=180,
+        blank=False,
+        null=False,
+    )
+    is_buyer = models.BooleanField(
+        verbose_name=_("Buyer"),
+        default=False,
+        help_text="Are you looking to buy a property",
+    )
+    is_seller = models.BooleanField(
+        verbose_name=_("Seller"),
+        default=False,
+        help_text="Are you looking to sell a property",
+    )
+    is_agent = models.BooleanField(
+        verbose_name=_("Agent"), default=False, help_text="Are you an agent?"
+    )
+    top_agent = models.BooleanField(verbose_name=_("Top Agent"), default=False)
+    ratings = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    num_reviews = models.IntegerField(
+        verbose_name=_("Number of reviews"), default=0, null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
